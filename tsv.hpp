4b97a4fd76d76fa72f01746a49f2151f04b0e06a
Marhau <214839282+Marhau-devoloper@users.noreply.github.com>
2026-05-08 02:18:34 -0500

    Update tsv.hpp
    Added string TSV_To_CSV(string path,bool Save_File = false, string Name = "Myfile.csv")
    
    Added string CSV_To_TSV(string path,bool Save_File = false, string Name = "Myfile.tsv")
    
    
    improved performance and fixed couple memory leaks

--- tsv.hpp
+++ tsv.hpp
@@ -25,7 +25,7 @@ list<string> OpenTSV(string Path){
 
 
 // searching inside of TSV and return it as string
-string SearchTSV_As_String(list<string> Data, string Query){
+string SearchTSV_As_String(list<string> &Data, string Query){
     string Matches;
     for (const auto& s : Data) {
     if (s.find(Query) != string::npos) {
@@ -35,7 +35,7 @@ string SearchTSV_As_String(list<string> Data, string Query){
     return Matches;
 }
 // searching inside of TSV and return it as list
-list<string> SearchTSV_As_List(list<string> Data, string Query){
+list<string> SearchTSV_As_List(list<string> &Data, string Query){
     list<string> Matches;
     for (const auto& s : Data) {
     if (s.find(Query) != string::npos) {
@@ -46,7 +46,7 @@ list<string> SearchTSV_As_List(list<string> Data, string Query){
 }
 
 // this function help us to speperate rows in collum
-list<string> Split_Rows(string sentence,  bool csv = false) {
+list<string> Split_Rows(string &sentence,  bool csv = false) {
   
     
     stringstream ss(sentence);
@@ -60,12 +60,12 @@ list<string> Split_Rows(string sentence,  bool csv = false) {
     if (csv == true ) {
         separator = ',';
     }else{
-        separator = '	';
+        separator = '\t';
     }
     // seperate a each part of string and put them in list 
     while (getline(ss, Whole_String , separator)) {
         size_t pos;
-        while ((pos = Whole_String .find("	")) != string::npos) {
+        while ((pos = Whole_String .find("\t")) != string::npos) {
             Whole_String .replace(pos, 3, "");
         }
         
@@ -76,9 +76,9 @@ list<string> Split_Rows(string sentence,  bool csv = false) {
 }
 
 // function to get specific row from list like 0 
-string GetRow(list<string> List, short int place){
+string GetRow(list<string>& List, size_t place){
     if (place >= List.size()) {
-        return "ERROR";
+    place = List.size() - 1;
     }
 
     auto it = List.begin();
@@ -96,7 +96,7 @@ string TSV_To_Json(string Path,bool save_file = false, bool csv = false ){
     //verbs is splited row of headder with will using as veriables in json 
     list<string>  verbs = Split_Rows(Headder,csv);
     // check how many verbs
-    short int number_of_verbs = verbs.size();
+    const short int number_of_verbs = verbs.size();
     
     //if Save_file == true we save result in file
     if (save_file == true){
@@ -104,83 +104,90 @@ string TSV_To_Json(string Path,bool save_file = false, bool csv = false ){
     ofstream MyFile("TSV.json");
     // addin [ at start of json
     MyFile << "[" << endl;
-    //main loop 
-    for (int B = 1; B < list_size; B++){
+    
+    // count  main loop
+    int counts;
+    
+    for (const auto& _ : lists){
         
         
-    //starting first element in json with {
-    MyFile << "{" << endl;
-    //we get a line
-    string TSV_Data = GetRow(lists,B);
-    // we split line into smaller pices
-    list<string> Value = Split_Rows(TSV_Data,csv);
-    //loop with write a json structure like "verb": value
-    for (int i = 0; i < number_of_verbs; i++)
-    {
-    //we get a line   
-    string key = GetRow(verbs, i);
-    // we split line into smaller pices
-    string value = GetRow(Value, i);
+        //starting first element in json with {
+        MyFile << "{" << endl;
+        //we get a line
+        string TSV_Data = GetRow(lists,counts);
+        // we split line into smaller pices
+        list<string> Value = Split_Rows(TSV_Data,csv);
+        //loop with write a json structure like "verb": value
+        for (int i = 1; i < number_of_verbs; i++)
+        {
+            //we get a line   
+            string key = GetRow(verbs, i);
+            // we split line into smaller pices
+            string value = GetRow(Value, i);
 
-    // clean key
-    key.erase(remove(key.begin(), key.end(), '\n'), key.end());
-    key.erase(remove(key.begin(), key.end(), '\r'), key.end());
-    key.erase(remove(key.begin(), key.end(), '"'), key.end());
-    // clean value
-    value.erase(remove(value.begin(), value.end(), '\n'), value.end());
-    value.erase(remove(value.begin(), value.end(), '\r'), value.end());
-    value.erase(remove(value.begin(), value.end(), '"'), value.end());
+            // clean key
+            key.erase(remove(key.begin(), key.end(), '\n'), key.end());
+            key.erase(remove(key.begin(), key.end(), '\r'), key.end());
+            key.erase(remove(key.begin(), key.end(), '"'), key.end());
+            // clean value
+            value.erase(remove(value.begin(), value.end(), '\n'), value.end());
+            value.erase(remove(value.begin(), value.end(), '\r'), value.end());
+            value.erase(remove(value.begin(), value.end(), '"'), value.end());
 
 
-    MyFile << "\"" << key << "\": \"" << value << "\"";
+            MyFile << "\"" << key << "\": \"" << value << "\"";
+            
+            //here we check if this element is last one if it last one we not adding , at end
+            if (i != number_of_verbs - 1)
+            //adding , at end
+                MyFile << ",";
+            //make indent   
+            MyFile << endl;
+        
+        }
+        //here we check if this element is last one if it last one we not adding }, at end
+        if (counts != list_size - 1){
+            //adding }, at end
+            MyFile << "}," << endl;
+        }else{
+            //adding regular }
+            MyFile << "}" << endl;
+        }
+        //make indent
+        MyFile << endl;
+        //move to next part
+        counts =+1;
+        }
+        
+        //finish a json with ]
+        MyFile << "]" << endl;
     
-    //here we check if this element is last one if it last one we not adding , at end
-    if (i != number_of_verbs - 1)
-    //adding , at end
-        MyFile << ",";
-    //make indent   
-    MyFile << endl;
-    
-    }
-    //here we check if this element is last one if it last one we not adding }, at end
-    if (B != list_size - 1){
-        //adding }, at end
-        MyFile << "}," << endl;
-    }else{
-        //adding regular }
-        MyFile << "}" << endl;
-    }
-    //make indent
-    MyFile << endl;
-    }
-    
-    //finish a json with ]
-    MyFile << "]" << endl;
-  
-    //save file
-    MyFile.close(); 
+        //save file
+        MyFile.close(); 
     }else{
     //if Save_file is false we just return json
     string json;
     //main loop
     json += "[\n" ;
-    for (int B = 1; B < list_size; B++){
+    int counts = 1;
+    for (const auto& _ : lists){
 
     //we use /n to make new line and [ { to start a json
     
     json += "{\n" ;
     //we getting a string line
-    string TSV_Data = GetRow(lists,B);
+    string TSV_Data = GetRow(lists,counts);
     //we dividing line into pieces
     list<string> Value = Split_Rows(TSV_Data,csv);
     //loop with write a json structure like "verb": value
-    for (int i = 0; i < number_of_verbs; i++)
+    int ijson;
+    for (const auto& _ : verbs)
     {
         
     //we get a line   
-    string key = GetRow(verbs, i);
+    string key = GetRow(verbs, ijson);
     // we split line into smaller pices
-    string value = GetRow(Value, i);
+    string value = GetRow(Value, ijson);
 
     // clean key
     key.erase(remove(key.begin(), key.end(), '\n'), key.end());
@@ -195,14 +202,14 @@ string TSV_To_Json(string Path,bool save_file = false, bool csv = false ){
     json += "\"" + key + "\": \"" + value + "\"";
     
     //here we check if this element is last one if it last one we not adding , at end  
-    if (i != number_of_verbs - 1)
+    if (ijson != number_of_verbs - 1)
     json += ",";
     //making new line
     json += "\n";
-    
+    ijson ++;
     }
     //here we check if this element is last one if it last one we not adding }, at end
-    if (B != list_size - 1){
+    if (counts != list_size - 1){
         //adding },/n
         json += "},\n";
     }else{
@@ -211,6 +218,7 @@ string TSV_To_Json(string Path,bool save_file = false, bool csv = false ){
         //finishing json with ]
         json += "]\n";
     }
+    counts =+1;
     }
     
     
@@ -221,3 +229,76 @@ string TSV_To_Json(string Path,bool save_file = false, bool csv = false ){
     return "Done";
 }
 
+
+//function with converts TSV to CSV, arguments, Path with is path to file in string, Save file is bool if set to true save CSV as file and final 3rd argument is FileName with file expension on default set to Myfile.csv
+string TSV_To_CSV(string path,bool Save_File = false, string Name = "Myfile.csv") {
+    list<string> File = OpenTSV(path);
+    
+    if (Save_File == true){
+        ofstream file(Name);
+        for (const string &line : File) {
+        string fixed = line;
+
+        // replace tabs with commas
+        replace(fixed.begin(), fixed.end(), '\t', ',');
+        
+        file << fixed << endl;
+    
+    }
+    
+    file.close();
+    return Name + " File Saved";
+    }else{
+        string result;
+        for (const string &line : File) {
+        string fixed = line;
+
+        // replace tabs with commas
+        replace(fixed.begin(), fixed.end(), '\t', ',');
+        
+        result += fixed + "\n";
+        
+    }
+    return result;
+    }
+    
+
+    
+}
+
+
+//function with converts CSV to TSV, arguments, Path with is path to file in string, Save file is bool if set to true save CSV as file and final 3rd argument is FileName with file expension on default set to Myfile.tsv
+string CSV_To_TSV(string path,bool Save_File = false, string Name = "Myfile.tsv") {
+    list<string> File = OpenTSV(path);
+    
+    if (Save_File == true){
+        ofstream file(Name);
+        for (const string &line : File) {
+        string fixed = line;
+
+        // replace tabs with commas
+        replace(fixed.begin(), fixed.end(), ',', '\t');
+        
+        file << fixed << endl;
+    
+    }
+    
+    file.close();
+    return Name + " File Saved";
+    }else{
+        string result;
+        for (const string &line : File) {
+        string fixed = line;
+
+        // replace tabs with commas
+        replace(fixed.begin(), fixed.end(), '\t', ',');
+        
+        result += fixed + "\n";
+        
+    }
+    return result;
+    }
+    
+
+    
+}
